436793842ba4196326468abffa06e1ccdb5c43b8
Giovanni ACCETTA <gbaccetta@gmail.com>
2021-04-27 19:39:10 -0400

    extracting tab router in a variable

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,15 +1,15 @@
 import 'package:auto_route/auto_route.dart';
+
 import 'group_screens/tab1_screen.dart';
 import 'group_screens/group_screen.dart';
 import 'group_screens/tab2_screen.dart';
 import 'group_screens/tab3_screen.dart';
 import 'user_screens/user_friends_screen.dart';
-
 import 'user_screens/user_details_screen.dart';
-import 'login_screens/login_screen.dart';
-import 'login_screens/signup_screen.dart';
 import 'user_screens/user_profile_screen.dart';
 import 'user_screens/user_screen.dart';
+import 'login_screens/login_screen.dart';
+import 'login_screens/signup_screen.dart';
 
 @AdaptiveAutoRouter(
   replaceInRouteName: 'Page,Route,Screen',
@@ -39,41 +39,7 @@ import 'user_screens/user_screen.dart';
         AutoRoute(path: '', page: UserProfileScreen),
         AutoRoute(path: 'details', page: UserDetailsScreen),
         AutoRoute(path: 'friends', page: UserFriendsScreen),
-        
-        //nested group route with a tab router
-        AutoRoute(
-          path: 'group/:id',
-          page: GroupScreen,
-          children: [
-            AutoRoute(
-              path: 'tab1',
-              name: 'GroupTab1Router',
-              page: EmptyRouterPage,
-              children: [
-                AutoRoute(path: '', page: Tab1Screen),
-                RedirectRoute(path: '*', redirectTo: ''),
-              ],
-            ),
-            AutoRoute(
-              path: 'tab2',
-              name: 'GroupTab2Router',
-              page: EmptyRouterPage,
-              children: [
-                AutoRoute(path: '', page: Tab2Screen),
-                RedirectRoute(path: '*', redirectTo: ''),
-              ],
-            ),
-            AutoRoute(
-              path: 'tab3',
-              name: 'GroupTab3Router',
-              page: EmptyRouterPage,
-              children: [
-                AutoRoute(path: '', page: Tab3Screen),
-                RedirectRoute(path: '*', redirectTo: ''),
-              ],
-            ),
-          ],
-        ),
+        groupTabRouter,
         // redirect all other paths
         RedirectRoute(path: '*', redirectTo: 'profile'),
       ],
@@ -85,3 +51,38 @@ import 'user_screens/user_screen.dart';
   ],
 )
 class $AppRouter {}
+
+//nested group route with a tab router
+const groupTabRouter = AutoRoute(
+  path: 'group/:id',
+  page: GroupScreen,
+  children: [
+    AutoRoute(
+      path: 'tab1',
+      name: 'GroupTab1Router',
+      page: EmptyRouterPage,
+      children: [
+        AutoRoute(path: '', page: Tab1Screen),
+        RedirectRoute(path: '*', redirectTo: ''),
+      ],
+    ),
+    AutoRoute(
+      path: 'tab2',
+      name: 'GroupTab2Router',
+      page: EmptyRouterPage,
+      children: [
+        AutoRoute(path: '', page: Tab2Screen),
+        RedirectRoute(path: '*', redirectTo: ''),
+      ],
+    ),
+    AutoRoute(
+      path: 'tab3',
+      name: 'GroupTab3Router',
+      page: EmptyRouterPage,
+      children: [
+        AutoRoute(path: '', page: Tab3Screen),
+        RedirectRoute(path: '*', redirectTo: ''),
+      ],
+    ),
+  ],
+);
